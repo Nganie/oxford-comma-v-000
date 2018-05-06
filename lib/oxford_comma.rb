f411689ade4cefbf915e245e07c6f4b568ed8be9
Nganie <ngan.tp.quan@gmail.com>
2018-05-06 12:04:34 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,5 +4,5 @@ if array length == 1
 elsif array length == 2
   return array.join("and")
 elsif array length == 3
-  return array.join(",")  array[2].split("and")
+  return "#{array[0]}, #{array[1]},  #{array[2]}.split("and")"
 end
