2cfd1e0cd5a0ccf30fd2b98a0570882c6e4a648b
Nganie <ngan.tp.quan@gmail.com>
2018-05-06 12:06:19 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,10 +1,10 @@
 def oxford_comma(array)
-if array length == 1
+if array.length == 1
   return array[0]
-elsif array length == 2
+elsif array.length == 2
   return array.join("and")
-elsif array length == 3
+elsif array.length == 3
   return "#{array[0]}, #{array[1]}, and #{array[2]}"
-else array length < 3
-  
+else array.length < 3
+
 end
